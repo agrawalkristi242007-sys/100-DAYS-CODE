adcf10879a1b080ad77f9661c38426d4fb1985a2
agrawalkristi242007-sys <agrawalkristi242007@gmail.com>
2025-10-26 23:00:07 +0530

    DAYS 42

---
+++ Q84.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+      printf("Name-KRISTI AGRAWAL \nSAP ID-590025883\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n"); 
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+    int i = 0;
+    while (str[i] != '\0' && str[i] != '\n') {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - ('a' - 'A');
+        i++;
+    }
+    printf("%s", str);
+    return 0;
+    getch();
+}
