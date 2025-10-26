63b08cc3fd599e96b3bbb5fa561f3ce49816bc6a
agrawalkristi242007-sys <agrawalkristi242007@gmail.com>
2025-10-26 16:43:22 +0530

    DAYS 8

---
+++ Q16.c
@@ -0,0 +1,50 @@
+/*Q16 (Conditional Statements)
+Write a program to input three numbers and find the largest among them using if–else.*/
+
+#include <stdio.h>
+
+int main() 
+{
+      printf("Name-KRISTI AGRAWAL \nSAP ID-590025883\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+	
+    int a, b, c, largest;
+
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Enter third number: ");
+    scanf("%d", &c);
+
+    
+    if (a > b) 
+    {
+        if (a > c) 
+        {
+            largest = a;
+        }
+         else 
+        {
+            largest = c;
+        }
+    }
+     else 
+    {
+        if (b > c) 
+        {
+            largest = b;
+        } 
+        else 
+        {
+            largest = c;
+        }
+    }
+
+    printf("The largest number is %d\n", largest);
+    getch();
+    return 0;
+}
