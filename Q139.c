714165075f8aedd3c38333b13592df0932b096ef
agrawalkristi242007-sys <agrawalkristi242007@gmail.com>
2025-11-24 09:04:48 +0530

    DAYS 89

---
+++ Q139.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+enum Numbers { A = 5, B = 10, C = 20, D = 30 };
+
+int main() 
+{
+      printf("Name-KRISTI AGRAWAL \nSAP ID-590025883\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    enum Numbers n;
+
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+    printf("D = %d\n", D);
+     getch();
+    return 0;
+}
