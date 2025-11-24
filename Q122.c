1cb4ae0b021e3ad2b51d5789d8e90436b4eb0a26
agrawalkristi242007-sys <agrawalkristi242007@gmail.com>
2025-11-24 08:58:43 +0530

    DAYS 72

---
+++ Q122.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+ {
+    printf("Name-KRISTI AGRAWAL \nSAP ID-590025883\ncourse-BSC-CS\nBATCH-B1\n");
+	printf("\n--------------------------------\n");
+    FILE *fp;
+    char line[200];
+
+    fp = fopen("info.txt", "r");
+
+    if (fp == NULL) {
+        printf("Error opening file!");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
